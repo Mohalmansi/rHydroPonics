7f49bf63e6c39b2b10fd6f7d6a8b59c5f6d7f4ee
Mohalmansi <109273294+Mohalmansi@users.noreply.github.com>
2023-11-07 02:57:25 +0200

    blink every 0.5 seconds

--- main/main.c
+++ main/main.c
@@ -13,9 +13,9 @@ void app_main(void)
     while (1)
     {
        gpio_set_level(GPIO_NUM_2,1);
-       vTaskDelay(1000);
+       vTaskDelay(100);
        gpio_set_level(GPIO_NUM_2,0);
-       vTaskDelay(1000);
+       vTaskDelay(100);
 
 
 
