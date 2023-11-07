ea35fdc055cd9ce439c08bd7a5cdb0abdbbbd077
Mohalmansi <109273294+Mohalmansi@users.noreply.github.com>
2023-11-07 02:13:59 +0200

    "this file works"

---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+
+
+
+void app_main(void)
+{
+		gpio_set_direction(GPIO_NUM_2,GPIO_MODE_OUTPUT);
+    while (1)
+    {
+       gpio_set_level(GPIO_NUM_2,1);
+       vTaskDelay(1000);
+       gpio_set_level(GPIO_NUM_2,0);
+       vTaskDelay(1000);
+
+
+
+    }
+}
