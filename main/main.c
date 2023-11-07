dd0260c60b0db938273faafa8176572c80a47a8f
Mohalmansi <109273294+Mohalmansi@users.noreply.github.com>
2023-11-07 03:05:50 +0200

    "blnk every 10 seconds"

--- main/main.c
+++ main/main.c
@@ -13,9 +13,9 @@ void app_main(void)
     while (1)
     {
        gpio_set_level(GPIO_NUM_2,1);
-       vTaskDelay(100);
+       vTaskDelay(1000);
        gpio_set_level(GPIO_NUM_2,0);
-       vTaskDelay(100);
+       vTaskDelay(1000);
 
 
 
